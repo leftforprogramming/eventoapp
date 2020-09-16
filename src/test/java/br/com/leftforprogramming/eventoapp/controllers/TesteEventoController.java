d08756311b659fb6d3c1de2abc70c8791cb7e814
Wesley Costa <wesferrcosta@gmail.com>
2020-09-16 17:49:32 -0300

    add: Metodo de teste

--- src/test/java/br/com/leftforprogramming/eventoapp/controllers/TesteEventoController.java
+++ src/test/java/br/com/leftforprogramming/eventoapp/controllers/TesteEventoController.java
@@ -3,11 +3,16 @@ package br.com.leftforprogramming.eventoapp.controllers;
 import com.google.gson.Gson;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import br.com.leftforprogramming.eventoapp.EventoappApplicationTests;
+import br.com.leftforprogramming.eventoapp.models.EventoModel;
 
 public class TesteEventoController extends EventoappApplicationTests {
     
@@ -21,5 +26,19 @@ public class TesteEventoController extends EventoappApplicationTests {
     public void setUp() {
         this.mockMvc = MockMvcBuilders.standaloneSetup(eventoController).build();
     }
+
+    @Test
+	public void cadastrarEvento() throws Exception {
+        EventoModel evento = new EventoModel();
+        evento.setNome("TESTE");
+        evento.setLocal("TESTE");
+        evento.setData("09/09/2020");
+        evento.setHorario("1212");
+		this.mockMvc
+            .perform(MockMvcRequestBuilders.post("")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(this.gson.toJson(evento)))
+            .andExpect(MockMvcResultMatchers.status().isCreated());
+	}
 
 }
