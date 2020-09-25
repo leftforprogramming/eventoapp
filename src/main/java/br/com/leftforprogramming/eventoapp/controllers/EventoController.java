1096900761bb40b593ebdcc61916b80ea37fbc7d
Wesley Costa <wesferrcosta@gmail.com>
2020-09-24 21:06:58 -0300

    Adicionado metodo que busca o evento pelo codigo

--- src/main/java/br/com/leftforprogramming/eventoapp/controllers/EventoController.java
+++ src/main/java/br/com/leftforprogramming/eventoapp/controllers/EventoController.java
@@ -2,6 +2,7 @@ package br.com.leftforprogramming.eventoapp.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -31,6 +32,14 @@ public class EventoController {
         ModelAndView mv = new ModelAndView("index");
         Iterable<EventoModel> eventos = this.eventoRepository.findAll();
         mv.addObject("eventos", eventos);
+        return mv;
+    }
+
+    @RequestMapping("/{codigo}")
+    public ModelAndView detalheEvento(@PathVariable("codigo") long codigo){
+        EventoModel evento = this.eventoRepository.findById(codigo);
+        ModelAndView mv = new ModelAndView("detalhesEvento");
+        mv.addObject("evento", evento);
         return mv;
     }
 }
--- src/main/java/br/com/leftforprogramming/eventoapp/repository/EventoRepository.java
+++ src/main/java/br/com/leftforprogramming/eventoapp/repository/EventoRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import br.com.leftforprogramming.eventoapp.models.EventoModel;
 
 public interface EventoRepository extends CrudRepository<EventoModel, String>{
+
+	EventoModel findById(long codigo);
     
 }
